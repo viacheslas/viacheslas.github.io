939dbb8064529ac1c4b6004f4e15f55d14309e89
viacheslas <krachmer@gmail.com>
2017-08-09 13:27:15 +0300

    final

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.mobile-menu').on('click', function(){
-        $('#menu').stop().slideToggle();
+    $('.mobile-menu .toggle').on('click', function(){
+        $('.navigation nav ul').stop().slideToggle();
     })
 });
